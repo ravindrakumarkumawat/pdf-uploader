cb6c264fb5968ac5a53e72798e5c52ee44594b20
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-30 15:34:49 +0530

    cors is added

--- server/app.js
+++ server/app.js
@@ -1,16 +1,28 @@
 const path = require('path')
 const express = require('express')
 const multer = require('multer')
-console.log()
+const cors = require('cors')
+
 const app = express()
 const port = process.env.PORT || 5000
 
 const publicDirectoryPath = path.join(__dirname, '../client/public')
+
+app.use(cors())
+app.use(express.json()) // req.body
+app.use(express.urlencoded({ extended: false }))
 app.use(express.static(publicDirectoryPath))
 
+// Routes
 
 
-
+// Start server
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/public')))
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/public/index.html'))
+  })
+}
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`)
