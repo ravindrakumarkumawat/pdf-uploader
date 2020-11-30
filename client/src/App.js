867fcca6518c8c262365042e069b9dbc2ca488ea
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-11-30 17:01:17 +0530

    upload container is added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,29 @@
-import "./App.css";
+import React from 'react'
+import {Progress} from 'reactstrap'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './App.css'
 
 function App() {
-  return (
+  return (<>
+    <div className="container">
+	      <div className="row">
+      	  <div className="offset-md-3 col-md-6">
+               <div className="form-group files">
+                <label>Upload Your File </label>
+                <input type="file" className="form-control" multiple />
+              </div>  
+              <div className="form-group">
+              <ToastContainer />
+              <Progress max="100" color="success"  >80%</Progress>
+        
+              </div> 
+              
+              <button type="button" className="btn btn-success btn-block" >Upload</button>
+
+	      </div>
+      </div>
+      </div>
     <form
       method="POST"
       action="http://localhost:5000/upload-profile-pic"
@@ -15,6 +37,7 @@ function App() {
         <input type="submit" name="btn_upload_profile_pic" value="Upload" />
       </div>
     </form>
+    </>
   )
 }
 
