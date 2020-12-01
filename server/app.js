aae910757d7f8c89c6c8ba7fdb808d19e6484036
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-12-01 15:40:37 +0530

    mongoose is connected

--- server/app.js
+++ server/app.js
@@ -63,3 +63,10 @@ app.listen(port, () => {
 
 
 // Setup for mongoose
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}, (err) => {
+  if (err) throw err
+  console.log('MongoDB connection established')
+})
