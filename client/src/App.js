8339a15c8a9dc014c1c494796211f5bcab89fd91
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-12-01 19:48:17 +0530

    pages and route setup is done

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import React, {} from 'react'
-import Upload from './components/upload/Upload'
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import './App.css'
+
+import Upload from './components/upload/Upload'
+import Home from './pages/Home'
+import Login from './components/auth/Login'
+import Register from './components/auth/Register'
+import Header from './components/layout/Header'
 
 function App() {
   return (
-    <Upload />
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/upload" component={Upload} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register} />
+      </Switch>
+    </BrowserRouter>    
   )
 }
 
