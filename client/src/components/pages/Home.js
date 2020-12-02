a66105fff7bb6acc0c69a45ab3feb6e4a5741a14
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-12-02 12:21:26 +0530

    Upload is added

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,15 +1,18 @@
 import React, { useContext } from "react"
 import { Link } from "react-router-dom"
 import UserContext from "../../context/UserContext"
+import Upload from '../upload/Upload'
 
 export default function Home() {
   const { userData } = useContext(UserContext)
 
   return (
     <div className="page">
-      {userData.user ? (<>
-        <h1>Welcome {userData.user.firstName}</h1>
-        <Link to="/upload">Upload</Link></>
+      {userData.user ? (
+        <>
+          <h1>Welcome {userData.user.firstName}</h1>
+          <Upload />
+        </>
       ) : (
         <>
           <h2>You are not logged in</h2>
