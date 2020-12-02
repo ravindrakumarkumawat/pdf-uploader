1fa35afd821fae38ac937e65cc36f6b09d13f963
ravindrakumarkumawat <ravindra.kumawat76@gmail.com>
2020-12-02 15:14:03 +0530

    file upload is done successfully

--- server/routes/uploadRouter.js
+++ server/routes/uploadRouter.js
@@ -3,12 +3,23 @@ const express = require('express')
 const router = express.Router()
 const pdfFilter = require('../helper/pdfFilter')
 const auth = require('../middleware/auth')
+const User = require('../models/userModel')
 
 
 // Multer storage
 const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-      cb(null, 'temp/uploads/')
+  destination: async function(req, file, cb) {            
+      cb(null, 'temp/uploads/')  
+      let user = await User.findOne({_id: req.baseUrl.split('/')[2]})
+      if (!user) {
+        return res.status(404).json({ error: 'user doesnt exist' })
+      }
+      user.pdfs.push({
+        fileName: file.originalname,
+        fileLocation: 'temp/uploads/'
+      })
+
+      await user.save()
   },
 
   filename: function(req, file, cb) {
@@ -19,6 +30,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage, fileFilter: pdfFilter }).array('file', 5)
 
 router.post('/upload', (req, res) => {
+  const userId = req.baseUrl.split('/')[2]
   upload(req, res, (err) => {
     if (err instanceof multer.MulterError) {
       // A Multer error occurred when uploading
